2905fabba9b2b1ea63f927afddf147b8ffd8c5aa
Aaron Tonatiuh Villanueva Guzmán <30881287+AaronVillanueva@users.noreply.github.com>
2017-09-12 08:09:11 -0500

    Update Números romanos.py

--- Números romanos.py
+++ Números romanos.py
@@ -3,10 +3,14 @@
 #Este programa hace cosas
 
 def final(var):
-  if: var ==0
-    q=var
-  elif: var <= 5
-    q=var*2
+  if: var => 1 and <=3
+    q=var*"I"
+  elif: var ==4
+    q=((var-3)*"I")+"V"
+  elif: var => 5 and <=8
+    q="V"+((var-5)*"I")
+  elif: var ==9
+    q="V"+((var-8)*"I")
   return(q)
 
 def Main():
