189a2cac0bf076ea1014b2dd1cd26e527e6cc4ca
Aaron Tonatiuh Villanueva Guzmán <30881287+AaronVillanueva@users.noreply.github.com>
2017-09-13 08:37:17 -0500

    Update Numeros_romanos.py

--- Numeros_romanos.py
+++ Numeros_romanos.py
@@ -3,23 +3,23 @@
 #Este programa imprime el número romano correspondiente a un número introducido por el usuario. Solamente es capaz de leer números del 1 al 10.
 
 def crearNumeroRomano(entero):
-  if: entero => 1 and <=3
+  if entero <=3:
     q=entero*"I"
-  elif: entero ==4
+  elif entero ==4:
     q=((entero-3)*"I")+"V"
-  elif: entero => 5 and <=8
+  elif entero <=8:
     q="V"+((entero-5)*"I")
-  elif: entero ==9
+  elif entero ==9:
     q=((entero-8)*"I")+"X"
-  elif: entero ==10
+  elif entero ==10:
     q="X"
   return(q)
 
 def Main():
   datosEntrada=int(input("Ingrese un número del 1 al 10"))
-  if datosEntrada<=0 and => 11
-    print ("error")
+  if 1<=datosEntrada<=10:
+    print("El número romano correspondiente a", datosEntrada,"es:",crearNumeroRomano(datosEntrada))    
   else:  
-    print("El número romano correspondiente a", datosEntrada,"es:",crearNumeroRomano(datosEntrada))
+    print ("error")
 
 Main()
