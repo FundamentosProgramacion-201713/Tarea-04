82416d72ec9d60c3d27fa44832309df73e964944
Aaron Tonatiuh Villanueva Guzmán <30881287+AaronVillanueva@users.noreply.github.com>
2017-09-13 11:43:53 -0500

    Update Mezclador_de_color.py

--- Mezclador_de_color.py
+++ Mezclador_de_color.py
@@ -1,18 +1,18 @@
 def mezcladordecolor(primercolor,segundocolor):
   if (primercolor == "rojo" or primercolor=="azul") and (segundocolor=="rojo" or segundocolor=="azul"):
-    x="Morado"
+    colormezclado="Morado"
   elif (primercolor == "rojo" or primercolor=="amarillo") and (segundocolor=="rojo" or segundocolor=="amarillo"):
-    x="Naranja"
+    colormezclado="Naranja"
   elif (primercolor == "azul" or primercolor=="amarillo") and (segundocolor=="azul" or segundocolor=="amarillo"):
-    x="verde"
-  return(x)
+    colormezclado="verde"
+  return(colormezclado)
 
 def Main():
   primario=input("Ingrese el primer color").lower()
-  secundario=(input("ingrese el segundo color")).lower()
-  coloresaceptados ="rojo","azul","amarillo"
-  if (primario== coloresaceptados) and (secundario==coloresaceptados):
+  secundario=input("ingrese el segundo color").lower()
+  if (primario== "rojo","azul","amarillo") and (secundario=="rojo","azul","amarillo"):
     print(mezcladordecolor(primario,secundario))
   else:
     print("error")
+    
 Main()
