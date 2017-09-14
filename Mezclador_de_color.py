812f4859b312570980987e365e4b4da1d0b22e4f
Aaron Tonatiuh Villanueva Guzmán <30881287+AaronVillanueva@users.noreply.github.com>
2017-09-14 10:53:49 -0500

    Update Mezclador_de_color.py

--- Mezclador_de_color.py
+++ Mezclador_de_color.py
@@ -2,11 +2,11 @@
 #Autor Aaron Tonatiuh Villanueva Guzmán
 #Este programa imprime el resultado de la mezcla de tres colores especificos sin importar su capitalización. Si recibe un color no preestablecido, regresa un mensaje de error.
 
-#Esta función procesa 
+#Esta función procesa los colores del usuario y procesa el color mezclado. Regresa una string con el resultado.
 def mezclarcolor(primercolor,segundocolor):
   if (primercolor == "rojo" and segundocolor == "azul") or (primercolor == "azul" and segundocolor == "rojo"):
     colormezclado="Morado"
-  elif (primercolor == "rojo" and segundocolor == "amarillo") or (primercolor == "blue" and segundocolor == "amarillo"):
+  elif (primercolor == "rojo" and segundocolor == "amarillo") or (primercolor == "azul" and segundocolor == "amarillo"):
     colormezclado="Naranja"
   elif (primercolor == "azul" and segundocolor == "amarillo") or (primercolor == "azul" and segundocolor == "amarillo"):
     colormezclado="Verde"
@@ -14,6 +14,7 @@ def mezclarcolor(primercolor,segundocolor):
     colormezclado="error"
   return(colormezclado)
 
+#Esta función lee dos colores introducidos por el usuario en texto. Es indiferente a capitalización y, a pesar de que lee cualquier color, utiliza a la función mezclar color para determinar si los colores son compatibles o si se debe imprimir un mensaje de error.
 def Main():
   primario=input("Ingrese el primer color").lower()
   secundario=input("ingrese el segundo color").lower()
