bb1c9fea61ca2ac411f3784122d84b8e2110674c
Aaron Tonatiuh Villanueva Guzmán <30881287+AaronVillanueva@users.noreply.github.com>
2017-09-15 08:09:32 -0500

    Update Venta_de_Software.py

--- Venta_de_Software.py
+++ Venta_de_Software.py
@@ -4,22 +4,22 @@
 
 #Esta función procesa el total a pagar según el número de unidades adquiridas y les aplica el descuento correspondiente.
 def calculartotal(numero):
-  elif numero <=9
+  if numero <=9:
     final=numero* 1500
-  elif numero <=19
+  elif numero <=19:
     final=(numero*1500)*.8
-  elif numero <=49
+  elif numero <=49:
     final=(numero*1500)*.7
-  elif numero <=99
+  elif numero <=99:
     final=(numero*1500)*.6
-  elif numero => 100
+  elif numero > 99:
     final=(numero*1500)*.5
   return(final)
 
 #Esta función lee el número de paquetes adquiridos y decide sí debe o no procesar el descuento.
 def Main():
   paquetes= int(input("Ingrese el número de paquetes adquiridos: "))
-  if paquetes <=0
+  if paquetes <= 0:
     print("error")
   else:
     print(calculartotal(paquetes))  
