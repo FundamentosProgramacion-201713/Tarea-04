6a64e1b251a2c6795a65071d28461e5121d923b9
luisalcantaralo <L.2009-a4785>
2017-09-16 14:06:48 -0500

    Tarea

--- ventas.py
+++ ventas.py
@@ -1,16 +1,13 @@
 #encoding: UTF-8
 #Autor: Luis Alfonso Alcántara López Ortega, A01374785
 
-#Función para calcular el subtotal
-def calcularSubtotal(num):
-    return num * 1500.0
-
 #Función para validar la entrada
 def validarEntrada(num):
     return num >= 0
 
-#Función para calcular el descuento
-def calcularDescuento(num):
+#Función para calcular el precio
+def calcularPrecio(num):
+    subtotal = 1500 * num
     if num >= 1 and num <= 9:
         descuento = 0
     elif num >= 10 and num <= 19:
@@ -23,24 +20,22 @@ def calcularDescuento(num):
         descuento = 50
     else:
         descuento = 0
-    return descuento
 
-#Función para calcular el precio
-def calcularPrecio(desc, subtotal):
-    precio = subtotal * ((100-desc)/100)
+    precio = subtotal * ((100-descuento)/100)
     return precio
 
+def cantidadDescontada(precio, num):
+    return (1500*num) - precio
 def main():
     numPaquetes = int(input("Ingresa el número de paquetes: "))
     validacion = validarEntrada(numPaquetes)
 
     if validacion:
-        subtotal = calcularSubtotal(numPaquetes)
-        descuento = calcularDescuento(numPaquetes)
-        precio = calcularPrecio(descuento, subtotal)
-        print("Subtotal: ", subtotal)
-        print("Descuento: ", descuento, "%", sep="")
-        print("Precio: ", precio)
+
+        precio = calcularPrecio(numPaquetes)
+        descuento = cantidadDescontada(precio, numPaquetes)
+        print("Descuento: ", "$", descuento, sep="")
+        print("Total a pagar: ", "$", precio, sep="")
     else:
         print("Número inválido")
 
