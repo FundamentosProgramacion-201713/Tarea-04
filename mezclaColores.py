b23ce2b780ddcf0ecb026b2108d5b16464d963cd
Sebastian <sebfut@hotmail.com>
2017-09-16 23:58:21 -0500

    PArte de tarea 4 (EDITADO)

---
+++ mezclaColores.py
@@ -0,0 +1,26 @@
+#encoding: UTF-8
+#sebastian Morales Martin
+#define el resultante de la mezcla de los colores
+a = "amarillo"
+r = "rojo"
+az = "azul"
+def main():
+    color1 = input("Ingrese el primer color (rojo, azul, amarillo): ")
+    color2 = input("Ingrese el segundo color (rojo, azul, amarillo): ")
+    if color1 and color2 != a and r and az:
+        print("ERROR")
+    mezcla = encontrarMezcla(color1, color2)
+    print(mezcla)
+
+def encontrarMezcla(color1, color2): #encuentra la mezcla entre los dos colores
+    if color1 == a and color2 == r:
+        mezcla = "Naranja"
+        return mezcla
+    elif color1 == r and color2 == az:
+        mezcla = "Morado"
+        return mezcla
+    elif color1 == az and color2 == a:
+        mezcla = "Verde"
+        return mezcla
+
+main()
