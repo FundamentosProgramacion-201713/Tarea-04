48047f58421731502881a8c4cf9c6604d511769c
raul <A01375407@itesm.mx>
2017-09-16 23:56:11 -0500

    el ejercico de las mezclas de los colores no lo pude resolver no le entendi

--- mezcladorDeColor.py
+++ mezcladorDeColor.py
@@ -5,11 +5,44 @@
 # y para eso debes de empezar con una mayuscula
 
 def combinarColores(c1,c2):
-    if c1=="Rojo"  and c2=="Azul" or c1=="Azul"  and c2=="rojo":
-        return ("Morado")
-    elif c1=="Rojo" and c2=="Amarillo":
-        return ("naranja")
-    elif c1== "Rojo" and c2=="Rojo":
-        return("Rojo")
+
+    if c1=="Rojo" and c2=="Azul" or c1=="Azul" and c2=="Rojo" :
+         combinacion=("MORADO")
+         return combinacion
 
 
+    elif c1== "Rojo" and c2 == "Amarillo" or c1=="Amarillo" and c2=="Rojo":
+         combinacion = ("Naranja")
+         return combinacion
+
+    elif c1 == "Azul" and c2 == "Amarillo" or c1=="Amarillo" and c2=="Azul":
+        combinacion = ("VERDE")
+        return combinacion
+
+    elif c1=="Rojo" and c2=="Rojo":
+        combinacion=("Rojo")
+        return combinacion
+
+    elif c1=="Azul" and c2=="Azul":
+        combinacion=("Azul")
+        return combinacion
+
+    elif c1=="Amarillo" and c2=="Amarillo":
+        combinacion=("Amarillo")
+        return combinacion
+
+def main():
+    c1=str(input("Escribe el primer color:"))
+    c2=str(input("Escribe el segundo color:"))
+    color1 = c1.upper()
+    color2= c2.lower()
+
+    if color1==("Rojo") or c1==("Azul") or c1==("Amarillo"):
+        print("El color es valido")
+        combinar=combinarColores(c1,c2)
+        print("El color combinado es:",combinar)
+    elif color2==("rojo") or("azul")or ("amarillo"):
+        print("solo se puede escribir con mayusculas la primera letra")
+
+
+main()
